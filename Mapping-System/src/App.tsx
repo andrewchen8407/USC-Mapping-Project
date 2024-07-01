6305f35ace6aa4274ae05a9a2b56bbc5e17732b3
andrewchen8407 <andrewchen8407@gmail.com>
2024-07-01 11:15:56 -0700

    Made a slew of changes.
    Changed the Reference Layers category to be a GroupLayer, with each sublayer being a unique reference layer.
    
    Changed the basemap from dark-gray to streets-vector so as to provide a less gloomy look and less contrast with the Street base layer.
    
    Changed the initial coordinates of focus as well as the zoom level to give a better overall view of Los Angeles County.
    
    Eliminated the redundant Esri World Geocoder and LA County CAMS SearchSources.
    
    Implemented a change that will append the query string of a search query to the URL. Still would like to work on removing the query string from the URL when the user no longer wishes to view the result of a query.
    
    Added a Home button widget that will return the user to the original coordinates and zoom level.
    
    Implemented a band-aid solution to eliminate the margins and unused space around the Esri map component. May have to clean up the code a bit more for this change.
    
    Threw in a CoordinateConversion widget just for fun. If the user stories I come up with in a future sprint planning don't involve the use of this widget, I will remove it.

--- Mapping-System/src/App.tsx
+++ Mapping-System/src/App.tsx
@@ -1,19 +1,16 @@
-// JSX allows html to be included in js/ts file. No need to tab between files.
+// JSX/TSX allows HTML to be included in JS/TS files. No need to tab between files.
 
 // React imports
 import { forwardRef, useEffect, useRef, useState } from "react";
 
-// Third-party library imports
-// import VectorTileLayer from "react-leaflet-vector-tile-layer";
-
 // ArcGIS core imports
 import Color from "@arcgis/core/Color.js";
-// import Collection from "@arcgis/core/core/Collection.js";
 // import * as reactiveUtils from "@arcgis/core/core/reactiveUtils.js";
 import Extent from "@arcgis/core/geometry/Extent.js";
 // import Basemap from "@arcgis/core/Basemap.js";
 import Map from "@arcgis/core/Map.js";
 import MapView from "@arcgis/core/views/MapView.js";
+import Home from "@arcgis/core/widgets/Home.js";
 
 // ArcGIS layer imports
 // import BaseTileLayer from "@arcgis/core/layers/BaseTileLayer.js";
@@ -31,7 +28,8 @@ import PictureMarkerSymbol from "@arcgis/core/symbols/PictureMarkerSymbol.js";
 import TextSymbol from "@arcgis/core/symbols/TextSymbol.js";
 
 // ArcGIS widget imports
-// import Expand from "@arcgis/core/widgets/Expand.js";
+import CoordinateConversion from "@arcgis/core/widgets/CoordinateConversion.js";
+import Expand from "@arcgis/core/widgets/Expand.js";
 import LayerList from "@arcgis/core/widgets/LayerList.js";
 // import LayerListItem from "@arcgis/core/widgets/LayerList/ListItem.js";
 // import ListItem from "@arcgis/core/widgets/LayerList/ListItem.js"; // duplicate import, should be removed
@@ -52,33 +50,22 @@ function formatContent(event: { graphic: { attributes: any; }; }) {
   let text = "";
   // Only display the attributes if they exist
   text += attributes.UseType ? `Use: ${attributes.UseType}<br>` : ""
-  text += attributes.SitusAddress ? `<br>Address:<br>${attributes.SitusAddress}<br>` : `Located in: `;
-  text += attributes.SitusCity  ? `${attributes.SitusCity}<br>` : ``;
+  text += attributes.SitusAddress ? `<br>Address:<br>${attributes.SitusAddress}<br>` : "Located in: ";
+  text += attributes.SitusCity  ? `${attributes.SitusCity}<br>` : "";
   let textElement = new TextContent({
     text: text
   });
   return [textElement];
 }
 
-// const wmtsBaseMap = new Basemap({
-//   baseLayers: [
-//     new WMTSLayer({
-//       url: "https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
-//       activeLayer: {
-//         id: "PICT-LARIAC6--LXMv769zxs",
-//         tileMatrixSetId: "GoogleMapsCompatible",
-//       },
-//     }),
-//   ],
-// });
-
 const streetMap = new MapImageLayer({
   visible: false,
+  title: "Street labels",
   url: "https://arcgis.gis.lacounty.gov/arcgis/rest/services/LACounty_Dynamic/Street_Labels/MapServer",
 });
 
 const assessorParcelMap = new FeatureLayer({
-  visible: false,
+  visible: true,
   title: "Assessor parcels",
   id: "assessor parcels",
   url: "https://cache.gis.lacounty.gov/cache/rest/services/LACounty_Cache/LACounty_Parcel/MapServer",
@@ -132,8 +119,8 @@ const communitiesMap = new FeatureLayer({
 
 const supervisorMap = new FeatureLayer({
   visible: false,
-  title: "Supervisorial units",
-  id: "supervisorial units",
+  title: "Supervisory units",
+  id: "supervisory units",
   url: "https://arcgis.gis.lacounty.gov/arcgis/rest/services/LACounty_Dynamic/Political_Boundaries/MapServer/1",
   labelingInfo: [
     new LabelClass({
@@ -161,45 +148,13 @@ const mobileHomeMap = new FeatureLayer({
   url: "https://assessor.gis.lacounty.gov/oota/rest/services/MAPPING/MobileHomes_Service_AMP/MapServer",
 });
 
-// export const createMapView = (
-//   url: string,
-//   coordinates: number[],
-//   mapRef: any
-// ) => {
-//   const wmtsBaseMap = new Basemap({
-//     baseLayers: [
-//       new WMTSLayer({
-//         url: "https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
-//         activeLayer: {
-//           id: "PICT-LARIAC6--LXMv769zxs",
-//           tileMatrixSetId: "GoogleMapsCompatible",
-//         }
-//       })
-//     ]
-//   });
-//   const view = new MapView({
-//     container: mapRef.current,  // added via ref
-//     map: new Map({
-//       basemap: wmtsBaseMap,
-//     }),
-//     center: coordinates,
-//     // zoom does not work because no LOD
-//     zoom: 11,
-//     constraints: {
-//       lods: LODS,
-//     },
-//     spatialReference: {
-//       wkid: 102100 //got it from bg assesor project
-//     }
-//   })
-// }
 
 type EsriMapProps = {
   url: string;
   coordinates: number[];
 };
 const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
-  // create a ref to element to be used as the map's container
+  // create a ref to element to be used as the map container
   // const [view,setView] = useState<MapView>()
 
   // View should be in state
@@ -209,8 +164,12 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
   const createMapView = (mapRef: any) => {
     const lods = LODS;
 
+    const url = new URL(window.location.href);
+    const query = url.searchParams.get("query") || "";
+    const sourceIndex = parseInt(url.searchParams.get("sourceIndex") || "0", 10);
+
     const map = new Map({
-      basemap: "dark-gray"
+      basemap: "streets-vector"
     });
 
     const view = new MapView({
@@ -222,8 +181,7 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
         fillOpacity: 0.2
       },
       center: coordinates,
-      // Zoom does not work because no LOD
-      zoom: 11,                                                                 
+      zoom: 9,             
       constraints: {
         lods: lods,
       },
@@ -232,62 +190,23 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
       },
     });
 
+    const homeWidget = new Home({
+      view: view
+    });
+    
+    view.ui.add(homeWidget, "top-left");
+
     const searchWidget = new Search({
       view: view,
       sources: [
-        new LocatorSearchSource({
-          url: "https://geocode.gis.lacounty.gov/geocode/rest/services/CAMS_Locator/GeocodeServer",
-          singleLineFieldName: "SingleLine",
-          resultSymbol: new PictureMarkerSymbol({
-            url: "/redpin.png",
-            width: 30,
-            height: 41,
-            yoffset: 14,
-            xoffset: 2,
-          }),
-          name: "LA County CAMS",
-          placeholder: "address",
-          suggestionsEnabled: true,
-        }),
-        // ESRI Geocoder
-
-        // hardcode search extent to la county for esri world locator
-        new LocatorSearchSource({
-          url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
-          
-          singleLineFieldName: "SingleLine",
-          name: "Esri World Geocoder",
-          placeholder: "place",
-          outFields: ["Addr_type"],
-          popupEnabled:false,
-          withinViewEnabled:true, // did nothing
-          resultSymbol: new PictureMarkerSymbol({
-            url: "/redpin.png",
-            width: 30,
-            height: 41,
-            yoffset: 14,
-            xoffset: 2,
-          }),
-          filter: {
-            geometry: new Extent({
-              xmin: -13400407.025469452,
-              ymin: 3854157.8250249373,
-              xmax: -12937519.219911631,
-              ymax: 4153533.5605398538,
-              spatialReference: {
-                wkid: 102100,
-              },
-            }),
-          },
-        }),
         new LayerSearchSource({
           searchFields: ["AIN"],
           layer: new FeatureLayer({
             url: "https://cache.gis.lacounty.gov/cache/rest/services/LACounty_Cache/LACounty_Parcel/MapServer/0",
-         
           }),
+          name: "Assessor Identification Number",
           outFields: ["*"],
-          placeholder: "AIN",
+          placeholder: "Find AIN (ten digits)",
           popupEnabled:true,
           popupTemplate: { 
             title: "{AIN}",
@@ -309,7 +228,6 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
           }  
         }),
       ],
-      searchAllEnabled: true,
     });
 
     /* formatting popup box */
@@ -323,16 +241,58 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
         xoffset: 2,
       });
     });
+
+    const updateURLParameters = (query: string, sourceIndex: number) => {
+      const url = new URL(window.location.href);
+      url.searchParams.set("query", query);
+      url.searchParams.set("sourceIndex", sourceIndex.toString());
+      window.history.pushState({}, "", url.toString());
+    };
+    
+    searchWidget.on("search-complete", (event) => {
+      const query = event.searchTerm;
+      let sourceIndex = event.activeSourceIndex;
+
+      if (sourceIndex === -1) {
+        const actualSource = event.results[0].source;
+        sourceIndex = searchWidget.sources.findIndex(source => source === actualSource) + 1;
+      }
+
+      const url = new URL(window.location.href);
+      url.searchParams.set("query", query);
+      url.searchParams.set("sourceIndex", sourceIndex.toString());
+      window.history.pushState({}, "", url.toString());
+
+      updateURLParameters(query, sourceIndex);
+    });
+
+    const params = new URLSearchParams(window.location.search);
+    const initialQuery = params.get("query") || "";
+    const initialSourceIndex = parseInt(params.get("sourceIndex") || "0", 10);
+
+    if (initialQuery) {
+      // Set the active source index before performing the search
+      searchWidget.activeSourceIndex = initialSourceIndex;
+      searchWidget.search(initialQuery);
+    }
+
     view.ui.add(searchWidget, {
       position: "top-left",
       index: 2,
     });
 
+    if (query) {
+      searchWidget.search(query);
+    }
+
     // Create sublayers for the Base Layer
     const subLayers = [
-      new MapImageLayer({
-        url: "https://cache.gis.lacounty.gov/cache/rest/services/LACounty_Cache/LACounty_Aerial_2014/MapServer",
-        id: "aerial 2014",
+      new WMTSLayer({
+        url: "https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
+        activeLayer: {
+          id: "PICT-LARIAC4--NQvK5pJZwy",
+          tileMatrixSetId: "GoogleMapsCompatible",
+        },
         title: "Aerial 2014",
         visible: false
       }),
@@ -348,19 +308,19 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
       new WMTSLayer({
         url: "https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
         activeLayer: {
-          id: "PICT-LARIAC5--SxmDvXHvYJ",
+          id: "PICT-LARIAC6--pCqXruF2NL",
           tileMatrixSetId: "GoogleMapsCompatible",
         },
-        title: "Aerial 2018",
+        title: "Aerial 2020",
         visible: false
       }),
       new WMTSLayer({
         url: "https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
         activeLayer: {
-          id: "PICT-LARIAC6--LXMv769zxs",
+          id: "PICT-LARIAC7--KCrSFBeqgG",
           tileMatrixSetId: "GoogleMapsCompatible",
         },
-        title: "Aerial 2022",
+        title: "Aerial 2023",
         visible: false
       }),
       new TileLayer({
@@ -379,7 +339,7 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
     });
 
     // Create standalone reference layers
-    const referenceLayers = [
+    const referenceLayersArray = [
       zoningMap,
       taxRateMap,
       supervisorMap,
@@ -390,8 +350,15 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
       assessorParcelMap
     ];
 
+    // Create the Base Layer with sublayers
+    const referenceLayers = new GroupLayer({
+      title: "Reference layers",
+      layers: referenceLayersArray,
+      visibilityMode: "independent"
+    });
+
     map.add(baseLayer);
-    map.addMany(referenceLayers);
+    map.add(referenceLayers);
 
     // Function to handle base layer selection
     const handleBaseLayerSelection = (selectedLayerId: string) => {
@@ -408,7 +375,6 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
       multipleSelectionEnabled: false,
       // listItemCreatedFunction: (event) => {
       //   const item = event.item;
-
       //   if (subLayers.some(layer => layer.id === item.layer.id)) {
       //     item.actionsSections = [
       //       [{
@@ -442,11 +408,24 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
       }
     });
 
-    view.ui.add(layerList, "top-right");
-
+    const layerListExpand = new Expand({
+      expandIcon: "layers",  // see https://developers.arcgis.com/calcite-design-system/icons/
+      // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+      view: view,
+      content: layerList
+    });
+    
+    view.ui.add(layerListExpand, "top-right");
+    
+    const coordinateConversion = new CoordinateConversion({
+      view: view
+    });
+    view.ui.add(coordinateConversion, "bottom-right");
+    
     return view;
   };
-  const [coordinates, setCoordinates] = useState([-118.2417, 34.0541]);
+  
+  const [coordinates, setCoordinates] = useState([-117.9367, 34.0041]);
   // const [map, setMap] = useState();
   // const [view, setView] = useState(null);
 
@@ -489,9 +468,7 @@ const EsriWithRef = forwardRef(function EsriMap(props: EsriMapProps, ref) {
   if (!ref) return null;
 
   return (
-    <>
-      <div style={{ height: 800 }} ref={ref}></div>
-    </>
+    <div style={{ height: '100vh', width: '100vw' }} ref={ref}></div>
   );
 });
 
@@ -499,9 +476,8 @@ export default function App() {
   // The phrase `export default` makes App() the default export for this package.
   // This means that App() can be imported using any alias.
 
-  // User can enter  address, place, or AIN, which finds location
+  // User can enter address, place, or AIN to obtian location
   // TODO: Find a way to pass in coordinates
-  // TODO: State to track the checked state of the checkbox
 
   const esriRef = useRef();
   return (
